9ad0bb17268a55df6d98772f6cdb6e093d78f7a7
George P. Corser <gpcorser@csis.svsu.edu>
2016-07-13 08:13:58 -0400

    files to upload (cats)

--- gpcorser/crud/create.php
+++ gpcorser/crud/create.php
@@ -1,7 +1,7 @@
 <?php 
 
-session_start();
-if (empty($_SESSION['name'])) header("Location: login.php"); // redirect
+	session_start();
+	if (empty($_SESSION['name'])) header("Location: login.php"); // redirect
 
 
 	# Consider three scenarios.
