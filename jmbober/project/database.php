405fcbbc5e63f887aba9395b1cc924264a3a2412
Jennifer M. Bober <jmbober@csis.svsu.edu>
2016-08-01 11:37:22 -0400

    jmbober

---
+++ jmbober/project/database.php
@@ -0,0 +1,41 @@
+<?php
+class Database 
+{
+
+	# added code below ...
+	private static $dbName = 'jmbober' ; 
+	private static $dbHost = 'localhost' ;
+	private static $dbUsername = 'jmbober';
+	private static $dbUserPassword = '549391';
+	# added code above ...
+	
+	private static $cont  = null;
+	
+	public function __construct() {
+		exit('Init function is not allowed');
+	}
+	
+	public static function connect()
+	{
+	   // One connection through whole application
+       if ( null == self::$cont )
+       {      
+        try 
+        {
+          self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".
+		  self::$dbName, self::$dbUsername, self::$dbUserPassword);  
+        }
+        catch(PDOException $e) 
+        {
+          die($e->getMessage());  
+        }
+       } 
+       return self::$cont;
+	}
+	
+	public static function disconnect()
+	{
+		self::$cont = null;
+	}
+}
+?>
